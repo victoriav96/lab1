5f51f9ee2daf1de35d3e7f1de2f2bee73cf8d0e1
victoriav96 <47906943+victoriav96@users.noreply.github.com>
2019-02-22 20:36:01 -0500

    Add files via upload

---
+++ Lab1/Lab1/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmAverageShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TxtInput = New System.Windows.Forms.TextBox()
+        Me.LblUnits = New System.Windows.Forms.Label()
+        Me.ToolTipInputBox = New System.Windows.Forms.ToolTip(Me.components)
+        Me.TxtDisplay = New System.Windows.Forms.TextBox()
+        Me.BtnEnter = New System.Windows.Forms.Button()
+        Me.BtnReset = New System.Windows.Forms.Button()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.LblOutput = New System.Windows.Forms.Label()
+        Me.LblDay = New System.Windows.Forms.Label()
+        Me.lblavg = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TxtInput
+        '
+        Me.TxtInput.Location = New System.Drawing.Point(148, 41)
+        Me.TxtInput.Name = "TxtInput"
+        Me.TxtInput.Size = New System.Drawing.Size(77, 22)
+        Me.TxtInput.TabIndex = 0
+        Me.ToolTipInputBox.SetToolTip(Me.TxtInput, "Input Units Here")
+        '
+        'LblUnits
+        '
+        Me.LblUnits.AutoSize = True
+        Me.LblUnits.Location = New System.Drawing.Point(88, 41)
+        Me.LblUnits.Name = "LblUnits"
+        Me.LblUnits.Size = New System.Drawing.Size(44, 17)
+        Me.LblUnits.TabIndex = 1
+        Me.LblUnits.Text = "Units:"
+        '
+        'TxtDisplay
+        '
+        Me.TxtDisplay.Location = New System.Drawing.Point(45, 77)
+        Me.TxtDisplay.MaximumSize = New System.Drawing.Size(300, 300)
+        Me.TxtDisplay.MinimumSize = New System.Drawing.Size(300, 300)
+        Me.TxtDisplay.Multiline = True
+        Me.TxtDisplay.Name = "TxtDisplay"
+        Me.TxtDisplay.ReadOnly = True
+        Me.TxtDisplay.Size = New System.Drawing.Size(300, 300)
+        Me.TxtDisplay.TabIndex = 100
+        Me.TxtDisplay.TabStop = False
+        '
+        'BtnEnter
+        '
+        Me.BtnEnter.Location = New System.Drawing.Point(12, 437)
+        Me.BtnEnter.Name = "BtnEnter"
+        Me.BtnEnter.Size = New System.Drawing.Size(114, 35)
+        Me.BtnEnter.TabIndex = 1
+        Me.BtnEnter.Text = "Enter"
+        Me.ToolTipInputBox.SetToolTip(Me.BtnEnter, "Enter Data into the Application to Calculate Average")
+        Me.BtnEnter.UseVisualStyleBackColor = True
+        '
+        'BtnReset
+        '
+        Me.BtnReset.Location = New System.Drawing.Point(148, 436)
+        Me.BtnReset.Name = "BtnReset"
+        Me.BtnReset.Size = New System.Drawing.Size(113, 35)
+        Me.BtnReset.TabIndex = 2
+        Me.BtnReset.Text = "Reset"
+        Me.ToolTipInputBox.SetToolTip(Me.BtnReset, "Reset Application and Erase Previous Data")
+        Me.BtnReset.UseVisualStyleBackColor = True
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Location = New System.Drawing.Point(278, 437)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(108, 36)
+        Me.BtnExit.TabIndex = 3
+        Me.BtnExit.Text = "Exit"
+        Me.ToolTipInputBox.SetToolTip(Me.BtnExit, "Close the Application")
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'LblOutput
+        '
+        Me.LblOutput.AutoSize = True
+        Me.LblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.LblOutput.Location = New System.Drawing.Point(91, 380)
+        Me.LblOutput.Margin = New System.Windows.Forms.Padding(7, 0, 7, 0)
+        Me.LblOutput.MaximumSize = New System.Drawing.Size(100, 100)
+        Me.LblOutput.MinimumSize = New System.Drawing.Size(210, 40)
+        Me.LblOutput.Name = "LblOutput"
+        Me.LblOutput.Size = New System.Drawing.Size(210, 40)
+        Me.LblOutput.TabIndex = 0
+        '
+        'LblDay
+        '
+        Me.LblDay.AutoSize = True
+        Me.LblDay.Location = New System.Drawing.Point(247, 41)
+        Me.LblDay.Name = "LblDay"
+        Me.LblDay.Size = New System.Drawing.Size(45, 17)
+        Me.LblDay.TabIndex = 7
+        Me.LblDay.Text = "Day 1"
+        '
+        'lblavg
+        '
+        Me.lblavg.AutoSize = True
+        Me.lblavg.Location = New System.Drawing.Point(44, 392)
+        Me.lblavg.Name = "lblavg"
+        Me.lblavg.Size = New System.Drawing.Size(37, 17)
+        Me.lblavg.TabIndex = 101
+        Me.lblavg.Text = "AVG"
+        '
+        'FrmAverageShipped
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(398, 492)
+        Me.Controls.Add(Me.lblavg)
+        Me.Controls.Add(Me.LblDay)
+        Me.Controls.Add(Me.LblOutput)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.BtnReset)
+        Me.Controls.Add(Me.BtnEnter)
+        Me.Controls.Add(Me.TxtDisplay)
+        Me.Controls.Add(Me.LblUnits)
+        Me.Controls.Add(Me.TxtInput)
+        Me.Name = "FrmAverageShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtInput As TextBox
+    Friend WithEvents LblUnits As Label
+    Friend WithEvents ToolTipInputBox As ToolTip
+    Friend WithEvents TxtDisplay As TextBox
+    Friend WithEvents BtnEnter As Button
+    Friend WithEvents BtnReset As Button
+    Friend WithEvents BtnExit As Button
+    Friend WithEvents LblOutput As Label
+    Friend WithEvents LblDay As Label
+    Friend WithEvents lblavg As Label
+End Class
---
+++ Lab1/Lab1/Form1.vb
@@ -0,0 +1,102 @@
+﻿''' <summary>
+''' Victoria Valdron 100548556
+''' NET DEVELOPMENT LAB 1
+''' 2019-01-25
+''' In this lab we have to create an application that takes in the input for 7 days, and calculate the average of all 7 inputs.
+''' </summary>
+
+
+
+Public Class FrmAverageShipped
+    'Variables'
+    Dim dayNumber As Integer = 1
+    Const TOTAL_DAYS As Integer = 7
+    Dim ifInteger As Integer
+    Dim day(6) As Integer
+    Dim average As Integer
+    Const MAX_NUM As Integer = 1000
+    Const MIN_NUM As Integer = 0
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        'ends the application'
+        Application.Exit()
+
+
+    End Sub
+
+    Private Sub BtnReset_Click(sender As Object, e As EventArgs) Handles BtnReset.Click
+        TxtInput.Clear()
+        TxtDisplay.Clear()
+        dayNumber = 1
+        LblDay.Text = "Day" & dayNumber
+
+
+    End Sub
+
+    Private Sub BtnEnter_Click(sender As Object, e As EventArgs) Handles BtnEnter.Click
+        'input validation'
+
+        If Integer.TryParse(TxtInput.Text, ifInteger) Then
+            ' Testing to see if the input is an integer '
+            If (ifInteger >= MIN_NUM And ifInteger <= MAX_NUM) Then
+
+
+
+
+
+                'excecute if above condition is true'
+                If (dayNumber < TOTAL_DAYS) Then
+
+                    'putting inputs into the array'
+                    day(dayNumber - 1) = ifInteger
+
+
+
+                    'Allow number of days to increase with each hit of the enter button'
+                    dayNumber += 1
+
+                    'Takes input of the txtInput and displays it into the txDisplay'
+                    TxtDisplay.AppendText(ifInteger & Environment.NewLine)
+
+
+                    'For 7th day, I created an else if so that once I press enter for the 7th day, it will automatically calculate the average in the bottom box'
+                ElseIf (dayNumber = TOTAL_DAYS) Then
+
+
+                    day(dayNumber - 1) = ifInteger
+
+                    TxtDisplay.AppendText(ifInteger & Environment.NewLine)
+
+                    'Calculating the average of the inputs'
+                    average = day.Sum / day.Count
+
+                    'Display average to the output label'
+                    LblOutput.Text = average
+
+                End If
+
+                LblDay.Text = "Day" & dayNumber
+
+                If (dayNumber > TOTAL_DAYS) Then
+
+                    LblDay.Text = "Day" & dayNumber - 1
+
+                End If
+                ' If the input is above 1000 or below 0 it will be considered out of range '
+            Else MessageBox.Show("Input out of range, please try again.")
+            End If
+
+            ' If the input is not an integer value, it will be an error '
+        Else MessageBox.Show("Input is not an integer, please try again.")
+
+        End If
+
+
+
+
+
+
+    End Sub
+
+
+End Class
